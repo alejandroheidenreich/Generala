4b499c04a7e2310a3ee3bca6961bc100c1160541
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-10-18 18:39:54 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Carta.cs
@@ -0,0 +1,12 @@
+﻿namespace Entidades
+{
+    public enum Palo
+    {
+        Espada, Basto, Oro, Copa
+    }
+    public static class Carta
+    {
+        static int numero;
+        static Palo palo;
+    }
+}
